ae727a64c9caa1cd67ff842755a961973497b126
Ekaterina Divina <ekaterinadivina@gmail.com>
2022-01-04 01:43:04 +0300

    fix

--- settings.py
+++ settings.py
@@ -1,7 +1,12 @@
 import logging
+import sys
+
+file_handler = logging.FileHandler(filename='tmp.log')
+stdout_handler = logging.StreamHandler(sys.stdout)
+handlers = [file_handler, stdout_handler]
 
 logging.basicConfig(
-    filename="logs.txt",
+    handlers=handlers,
     level=logging.INFO,
     format="\n%(asctime)s | %(levelname)s | %(message)s",
     datefmt="%Y.%m.%d %H:%M:%S",
